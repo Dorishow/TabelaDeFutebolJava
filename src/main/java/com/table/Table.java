56766b0b5b539b8fa34a9929325ce45b232e1dab
João Almeida <69915069+Dorishow@users.noreply.github.com>
2022-02-06 11:56:41 -0300

    feat: Classe main de controle que apenas chama os métodos

--- src/main/java/com/table/Table.java
+++ src/main/java/com/table/Table.java
@@ -2,34 +2,28 @@ package com.table;
 
 import com.table.enums.MatchResult;
 import com.table.matches.Match;
-import com.table.matches.FileHandler;
 import com.table.teams.Team;
-import com.table.utils.services.FileContentGetter;
 import com.table.utils.filesGenerator.FilesWriter;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+@Getter
+@Setter
 public class Table {
-    private static Set<String> teamsList = new HashSet<>();
-    private static SortedTable sortedTable = new SortedTable();
+    private Set<String> teamsList = new HashSet<>();
+    private SortedTable sortedTable = new SortedTable();
 
-    public static void main(String[] args) {
-
-        FileHandler fileContent = FileContentGetter.readFromFile("santander811matchesResult.csv");
-        teamsList = FileContentGetter.getMatchHostsFromFile(fileContent);
-        Map<String, List<Match>> allMatchesOfEachTeam = new HashMap<>();
-        teamsList.forEach(team -> allMatchesOfEachTeam.put(team, fileContent.getMatchesFromFileContent().filterByTeam(team)));
-        allMatchesOfEachTeam.forEach((team, matches) -> generateFilesByTeam(team, matches));
-        generateTableFile();
-    }
-
-    private static void generateTableFile() {
+    public void generateTableFile() {
         final String[] sortedTableFileContent = {""};
         sortedTable.getTable().forEach(team -> sortedTableFileContent[0] += team + "\n");
         FilesWriter.Write("src/main/files/table/table.csv", sortedTableFileContent[0]);
     }
 
-    private static void generateFilesByTeam(String team, List<Match> matches){
+    public void generateFilesByTeam(String team, List<Match> matches){
         Team newTeam = Team.builder().name(team).points(0).wins(0).draws(0).loses(0).build();
         final String[] fileContent = {""};
         matches.forEach(match -> {
