bb495a4773d7024262204e4900a7f9fe341bf0ab
João Almeida <69915069+Dorishow@users.noreply.github.com>
2022-02-05 21:43:26 -0300

    feat: generating files for the teams

--- src/main/java/com/table/Table.java
+++ src/main/java/com/table/Table.java
@@ -4,6 +4,7 @@ import com.table.enums.MatchResult;
 import com.table.matches.Match;
 import com.table.matches.FileHandler;
 import com.table.teams.Team;
+import com.table.utils.writeFiles.FilesWriter;
 
 import java.util.*;
 
@@ -17,22 +18,22 @@ public class Table {
         Map<String, List<Match>> allMatchesOfEachTeam = new HashMap<>();
         teamsList.forEach(team -> allMatchesOfEachTeam.put(team, fileContent.getMatchesFromFileContent().filterByTeam(team)));
         allMatchesOfEachTeam.forEach((team, matches) -> generateFileByTeam(team, matches));
-        System.out.println("---------------------------------");
-        sortedTable.table.forEach(System.out::println);
-        System.out.println("---------------------------------");
+//        System.out.println("---------------------------------");
 //        sortedTable.table.forEach(System.out::println);
-//        teamsList.forEach(System.out::println);
+//        System.out.println("---------------------------------");
     }
 
     private static void generateFileByTeam(String team, List<Match> matches){
         Team newTeam = Team.builder().name(team).points(0).wins(0).draws(0).loses(0).build();
+        final String[] fileContent = {""};
         matches.forEach(match -> {
-//            Add match to the Team file
+            fileContent[0] += match + "\n\r";
             MatchResult result = newTeam.handleMatch(match);
             if(result == MatchResult.DRAW) newTeam.addDraw();
             else if(result == MatchResult.WIN) newTeam.addWin();
             else if(result == MatchResult.LOOSE) newTeam.addLoose();
         });
+        FilesWriter.Write("src/main/files/".concat(team).concat(".csv"), fileContent[0]);
         System.out.println(newTeam);
         sortedTable.table.add(newTeam);
     }
